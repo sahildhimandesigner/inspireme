c0d6d106121f56a5a0c2f97047eb72dba7da8dec
Sarabjeet Singh <sarabsingh212@gmail.com>
2019-08-30 21:26:22 +0530

    move styles in a separate file

---
+++ src/Components/Quotes/Quote.style.js
@@ -0,0 +1,21 @@
+export const quoteStyle = {
+    quoteContainer: {
+        backgroundColor: '#f9f9f9',
+        border: '#e1e1e1 solid 1px',
+        padding: '20px',
+        margin: '10px auto 10px auto',
+        maxWidth: '800px',
+        borderRadius: '5px',
+        textAlign: 'left'
+    },
+    authourName: {
+        margin: '0',
+        padding: '0 0 10px 0',
+        fontSize: '18px',
+    },
+    quoteDetail: {
+        margin: '0',
+        padding: '0 0 10px 0',
+        lineHeight: '18px'
+    }
+};
--- src/Components/Quotes/Quotes.js
+++ src/Components/Quotes/Quotes.js
@@ -1,27 +1,6 @@
 import React from 'react'
 import withStyle from 'react-jss'
-
-const comStyle = {
-    quoteContainer : {
-        backgroundColor:'#f9f9f9',   
-        border:'#e1e1e1 solid 1px',
-        padding:'20px',
-        margin: '10px auto 10px auto',
-        maxWidth:'800px',
-        borderRadius:'5px',
-        textAlign:'left'
-    },
-    authourName: {
-        margin:'0',
-        padding:'0 0 10px 0',
-        fontSize:'18px',
-    },
-    quoteDetail:{
-        margin:'0',
-        padding:'0 0 10px 0',
-        lineHeight:'18px'
-    }
-}
+import { quoteStyle } from './Quote.style';
 
 const DailyQuote = ({classes, ...props}) => {    
     return(
@@ -33,4 +12,4 @@ const DailyQuote = ({classes, ...props}) => {
     )
 }
 
-export default withStyle(comStyle)(DailyQuote);
+export default withStyle(quoteStyle)(DailyQuote);
